7868fafb545e615c7d13649d505b7d054c1e25ec
Nicole Nikas <nnikas@fordham.edu>
2017-05-12 14:03:24 -0700

    Create condition

---
+++ condition.js
@@ -0,0 +1,12 @@
+function keyPressed(){
+
+var s = document.createElement("script");
+s.type = "text/javascript";
+if (key === 'w'){
+			s.src = "visual/spectrum.js";
+	}
+	else{
+			s.src = "visual/waveform.js";
+	}
+ 
+}
