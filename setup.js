1f5b0c3b47999e0395e024738543e9bee630eb5f
Nicole Nikas <nnikas@fordham.edu>
2017-05-12 13:55:54 -0700

    Update setup

--- setup.js
+++ setup.js
@@ -1,22 +1,31 @@
 "use strict";
 
+/* Tried creating a condition where pressing different keys switched between visual js files but couldn't get it to function
 var body = document.getElementsByTagName("body")[0];
 var s = document.createElement("script");
 s.type = "text/javascript";
+    else if (key === 'w'){
+			s.src = "visual/spectrum.js";
+	}
+	else{
+			s.src = "visual/waveform.js";
+	}
+   body.appendChild(s); 
+*/
 
-	
-
+// Load songs
 const AUDIO_SOURCES = [
     'What So Not - High You Are (Branchez Remix).mp3',
 ];
 
 var audioFile, analyzer;
-
+//Randomly choose a song  
 function preload() {
     var audioSrc = AUDIO_SOURCES[Math.floor(Math.random() * AUDIO_SOURCES.length)];
     audioFile = loadSound(audioSrc);
 }
 
+//if space is pressed play/pause the song
 function keyPressed() {
 	
     if (key === " ") {
@@ -27,12 +36,5 @@ function keyPressed() {
             audioFile.play();
         }
     }   
-    else if (key === 'w'){
-			s.src = "visual/spectrum.js";
-	}
-	else{
-			s.src = "visual/waveform.js";
-	}
-   body.appendChild(s); 
 }
 
